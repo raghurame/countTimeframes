185b1ce4966ec69d7d92d9b519df36a7f0705a9e
Raghuram Elancheran <83778193+raghurame@users.noreply.github.com>
2022-06-28 17:23:42 +0530

    calculating the number of perfect and imperfect timeframes

--- countTimeframes.c
+++ countTimeframes.c
@@ -10,7 +10,8 @@ int main(int argc, char const *argv[])
 	char *pipeString;
 	pipeString = (char *) malloc (500 * sizeof (char));
 
-	int nTimeframes = 0;
+	int nTimeframes = 0, nLines = 0, nAtoms = 0, nLinesPerTimeframe, nPerfectTimeframe = 0, nImperfectTimeframe = 0;
+	int isAtomLine = 0;
 
 	if (strstr (argv[1], ".xz")) {
 		snprintf (pipeString, 500, "xzcat %s", argv[1]);
@@ -22,15 +23,33 @@ int main(int argc, char const *argv[])
 
 	while (fgets (lineString, 1000, input))
 	{
-		if (strstr (lineString, "ITEM: TIMESTEP"))
-		{
+		nLines++;
+		nLinesPerTimeframe++;
+
+		if ((isAtomLine == 1) && (nAtoms == 0)) {
+			sscanf (lineString, "%d\n", &nAtoms);
+			isAtomLine = 0; }
+
+		if (strstr (lineString, "ITEM: TIMESTEP")) {
 			nTimeframes++;
 			printf("Counting timeframes... %d                \r", nTimeframes);
 			fflush (stdout);
-		}
+
+			if ((nLinesPerTimeframe == (nAtoms + 9)) && (nAtoms > 0) && (nLinesPerTimeframe > 10))
+				nPerfectTimeframe++;
+
+			else if ((nLinesPerTimeframe != (nAtoms + 9)) && (nAtoms > 0) && (nLinesPerTimeframe > 10))
+				nImperfectTimeframe++;
+
+			nLinesPerTimeframe = 0; }
+
+		if (strstr (lineString, "ITEM: NUMBER OF ATOMS"))
+			isAtomLine = 1;
 	}
 
 	printf("Number of timeframes in the input file: %d\n", nTimeframes);
+	printf("Total number of lines: %d\nNumber of timeframes calculated from total number of lines: %d (%d)\n", nLines, nLines / (nAtoms + 9), nLines % (nAtoms + 9));
+	printf("Perfect timeframes: %d\nImperfect timeframes: %d\n\n", nPerfectTimeframe, nImperfectTimeframe);
 
 	fclose (input);
 	return 0;
